41d87efc277ab9b5cc9c0f55b0f414d358dfe442
daisy-oss <77732121+daisy-oss@users.noreply.github.com>
2021-10-07 13:29:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+    
+        canvas=createCanvas(1240,336);
+        canvas.parent('canvas');
+        instializeInSetup(mario);
+    
+}
+function draw(){
+
+}
+function preload(){
+    
+}
